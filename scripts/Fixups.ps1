1bfa35e6ec514b2fd3913a94765dec7e1c4e9cd3
June Rhodes <jrhodes@redpoint.games>
2018-10-29 22:40:58 +1100

    Update Fixups.ps1

--- scripts/Fixups.ps1
+++ scripts/Fixups.ps1
@@ -1,3 +1,5 @@
+$ErrorActionPreference = "Stop"
+
 Write-Output "Fix up for VS version..."
 if (!(Test-Path "C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio")) {
   Set-Location "C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio"
@@ -5,7 +7,9 @@ if (!(Test-Path "C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio")) {
 }
 
 Write-Output "Fix up for VS APPX..."
-if (!(Test-Path "C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio\v12.0\AppxPackage\PDBCopy.exe")) {
-  mkdir "C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio\v12.0\AppxPackage\"
-  Copy-Item 'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\pdbcopy.exe'  'C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio\v12.0\AppxPackage\'
+if (Test-Path 'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\pdbcopy.exe') {
+  if (!(Test-Path "C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio\v12.0\AppxPackage\PDBCopy.exe")) {
+    mkdir "C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio\v12.0\AppxPackage\"
+    Copy-Item 'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\pdbcopy.exe'  'C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio\v12.0\AppxPackage\'
+  }
 }
