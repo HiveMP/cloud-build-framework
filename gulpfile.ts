649c32aba852527cbbca05be7b747d0265dbd404
June Rhodes <jrhodes@redpoint.games>
2018-10-29 21:27:17 +1100

    Add Gulp CLI

--- gulpfile.ts
+++ gulpfile.ts
@@ -10,30 +10,28 @@ const config = require("./config.json") as ConfigSchema;
 const workingDirectory = join(__dirname, "build", config["build-id"]);
 
 gulp.task("init-working-directory", async () => {
-  await mkdirpAsync(workingDirectory);
-  if (existsSync(join(workingDirectory, ".git"))) {
-    await execAsync("git", ["init"], workingDirectory);
-  }
+	await mkdirpAsync(workingDirectory);
+	if (!existsSync(join(workingDirectory, ".git"))) {
+		await execAsync("git", ["init"], workingDirectory);
+	}
 
-  let sourceUrl = config.source;
-  if (sourceUrl.startsWith("./") || sourceUrl.startsWith("../")) {
-    // This is a local file, need to convert to absolute path.
-    sourceUrl = resolve(join(__dirname, sourceUrl));
-  }
+	let sourceUrl = config.source;
+	if (sourceUrl.startsWith("./") || sourceUrl.startsWith("../")) {
+		// This is a local file, need to convert to absolute path.
+		sourceUrl = resolve(join(__dirname, sourceUrl));
+	}
 
-  // Fetch target branch.
-  await execAsync(
-    "git",
-    ["fetch", "--depth=1", sourceUrl, c(config.branch, "master")],
-    workingDirectory
-  );
+	// Fetch target branch.
+	await execAsync("git", ["fetch", "--depth=1", sourceUrl, c(config.branch, "master")], workingDirectory);
 
-  // Checkout and switch to target branch.
+	// Checkout and switch to target branch.
+	/*
   await execAsync(
     "git",
     ["checkout", "-fB", "master", "FETCH_HEAD"],
     workingDirectory
   );
+  */
 });
 
 gulp.task("build-ue4-custom", gulp.series("init-working-directory"));
--- index.ts
+++ index.ts
@@ -1,28 +1,7 @@
-import * as gulp from "gulp";
 import { ConfigSchema } from "./src/configSchema";
-import { mkdirpAsync } from "./src/mkdirp";
-import { join } from "path";
-import { execAsync } from "./src/execAsync";
-import { existsSync } from "fs";
 
-const config = require("./config.example.json") as ConfigSchema;
-const workingDirectory = join(__dirname, config["build-id"]);
+const config = require("./config.json") as ConfigSchema;
 
-gulp.task("init-working-directory", async () => {
-  await mkdirpAsync(workingDirectory);
-  if (existsSync(join(workingDirectory, ".git"))) {
-    await execAsync("git", ["init"], workingDirectory);
-  }
-
-  // Fetch target branch.
-  await execAsync(
-    "git",
-    ["fetch", config.source, config.branch],
-    workingDirectory
-  );
-});
-
-gulp.task("build-ue4-custom", gulp.series("init-working-directory"));
-
-// Run the target task.
-gulp.series(config["gulp-task"]);
+process.argv = ["node", "gulp", config["gulp-task"]];
+const run = require("gulp-cli");
+run();
