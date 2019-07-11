c9059adc007f57d8d3491d3f30363fe53cd1e2db
June Rhodes <june@networknext.com>
2019-07-11 11:13:48 -0700

    Updates for UE4.22

--- scripts/Fixups.ps1
+++ scripts/Fixups.ps1
@@ -1,15 +1,2 @@
 $ErrorActionPreference = "Stop"
 
-Write-Output "Fix up for VS version..."
-if (!(Test-Path "C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio")) {
-  Set-Location "C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio"
-  mklink /J v12.0 v14.0
-}
-
-Write-Output "Fix up for VS APPX..."
-if (Test-Path 'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\pdbcopy.exe') {
-  if (!(Test-Path "C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio\v12.0\AppxPackage\PDBCopy.exe")) {
-    mkdir "C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio\v12.0\AppxPackage\"
-    Copy-Item 'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\pdbcopy.exe'  'C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio\v12.0\AppxPackage\'
-  }
-}
